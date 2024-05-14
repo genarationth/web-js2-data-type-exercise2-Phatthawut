b6776e8f952c11196bd89e8cd5244aa94926d4f8
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-05-14 11:58:24 +0700

    Create script exercise 2

---
+++ script.js
@@ -0,0 +1,6 @@
+const birthYear = 1990;
+const furtureYears = 2049;
+
+let ageEx2 = furtureYears - birthYear;
+
+console.log(`you were born in ${birthYear}, then in ${furtureYears} you’ll be ${ageEx2}`);
